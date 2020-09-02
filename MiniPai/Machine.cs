968e1149cf208cb42fc032504d4b448f178bc7bf
cuishuning <cuishuning@gmail.com>
2020-09-02 16:56:13 +0800

    master

--- MiniPai/Machine.cs
+++ MiniPai/Machine.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 
 /// <summary>
-/// 该机器有16个通用寄存器
+///机器体系结构：
+///这种机器有16个通用寄存器，编号为0 ~F(十六进制表示）。每个寄存器的长度为1字节（8位）。
+///为了在指令中标识寄存器，每个寄存器被赋予了唯一的4位二进制模式，用于代表其寄存器号。
+///于是，寄存器0由0000(十六进制0)标识，寄存器4由0100(十六进制4)标识。
+///机器主存中有256个单元，每个单元被赋予一个范围为0 ~255的整数地址。因此，一个地址
+///可以由00000000~11111111(或者是十六进制值的00~FF)的一个8位二进制模式来表示。
 /// </summary>
 namespace MiniPai
 {
@@ -19,6 +24,9 @@ namespace MiniPai
         public byte Code { get; set; }    //内存的值
 
     }
+    /// <summary>
+    /// 每条机器指令都是2字节长：前面的4位是操作码，后面的12位组成操作数字段。
+    /// </summary>
 
     public struct RunInfo
     {
@@ -141,30 +149,54 @@ namespace MiniPai
         public bool RunInstructions()
         {
             runInfo.preAddress = PCAddress;   //存储当前指令地址
+            //下面用十六进制记数法表示的指令及简要说明。字母R、S及T在表示寄存器标识符的那些
+            //字段处用来替代十六进制数字，并且因指令的具体应用而异。字母X及Y用来在变量字段替代十
+            //六进制数字，而不是代表寄存器。
+            //指令用CRXY的形式表示，其中C是指令码
             switch (parseInstruct.operationCode)
             {
-                case 1:   
+           
+                case 1:
+                    //指令：1RXY
+                    //在地址为XY的存储单元中找到的二进制数加载（LOAD)到寄存器R
+                    //例：14A3将使得地址为A3的存储单元的内容放入寄存器4
+
                     regList[parseInstruct.regAddress01].Value = memList[parseInstruct.memAddress].Code;
                     runInfo.instructionsAccout++;
                     runInfo.regmem = RunInfo.RegMem.REGISTER;
                     runInfo.addressNum = parseInstruct.regAddress01;
                     PCAddress += 2;
                     break;
+
                 case 2:
+                    //指令：2RXY
+                    //将二进制数XY存入（LOAD)寄存器R
+                    //例：20A3将使得值A3放入寄存器0
+
                     regList[parseInstruct.regAddress01].Value = parseInstruct.memAddress;
                     runInfo.instructionsAccout++;
                     runInfo.regmem = RunInfo.RegMem.REGISTER;
                     runInfo.addressNum = parseInstruct.regAddress01;
                     PCAddress += 2;
                     break;
+
                 case 3:
+                    //指令： 3RXY
+                    //将寄存器R中的位模式存储（STORE)在地址为XY的存储单元中
+                    //例：35B1将使得寄存器5中的内容放入地址为B1的存储单元
+
                     memList[parseInstruct.memAddress].Code = regList[parseInstruct.regAddress01].Value;
                     runInfo.instructionsAccout++;
                     runInfo.regmem = RunInfo.RegMem.MEMORY;
                     runInfo.addressNum = parseInstruct.memAddress;
                     PCAddress += 2;
                     break;
+
                 case 4:
+                    //指令： 40RS
+                    //将寄存器R中的值复制到寄存器S
+                    //40A4将使得寄存器A的内容复制到寄存器4
+
                     regList[parseInstruct.regAddress03].Value = regList[parseInstruct.regAddress02].Value;
                     runInfo.instructionsAccout++;
                     runInfo.regmem = RunInfo.RegMem.REGISTER;
@@ -172,6 +204,12 @@ namespace MiniPai
                     PCAddress += 2;
                     break;
                 case 5:
+                    //指令： 5RST
+                    //将寄存器S及寄存器T的位模式作为二进制补码表示相加（ADD),将求
+                    //和结果存放在寄存器R中
+                    //例：5726将使得寄存器2和寄存器6中的二进制数值相加，并将和存放在
+                    //寄存器7中
+
                     sbyte sbyteReg01 = (sbyte)regList[parseInstruct.regAddress02].Value;
                     sbyte sbytereg02 = (sbyte)regList[parseInstruct.regAddress03].Value;
                     sbyte result = (sbyte)(sbyteReg01 + sbytereg02);
@@ -181,7 +219,14 @@ namespace MiniPai
                     runInfo.addressNum = parseInstruct.regAddress01;
                     PCAddress += 2;
                     break;
+
                 case 6:
+                    //指令： 6RST
+                    //将寄存器S及寄存器T的位模式作为浮点表示值相加（ADD),并将浮点
+                    //结果存放在寄存器R中
+                    //例：634E将使得寄存器4和寄存器E中的浮点值相加，并将结果存放在寄
+                    //存器3中
+
                     float reg02 = ParseFloat(regList[parseInstruct.regAddress02].Value);
                     float reg03 = ParseFloat(regList[parseInstruct.regAddress03].Value);
                     float reg01 = reg02 + reg03;
@@ -191,7 +236,10 @@ namespace MiniPai
                     runInfo.addressNum = parseInstruct.regAddress01;
                     PCAddress += 2;
                     break;
+
                 case 7:
+                    //指令： 7RST
+                     
                     regList[parseInstruct.regAddress01].Value =
                      (byte)(regList[parseInstruct.regAddress02].Value | regList[parseInstruct.regAddress03].Value);
                     runInfo.instructionsAccout++;
